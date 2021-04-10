539a397abb714ed6adcbaceae7568c1309eac98c
코딩티처지니 <kj_cho@idealidea.co.kr>
2021-04-10 14:35:50 +0900

    회원가입 실제 실행 => 서버응답 대응

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/SignUpActivity.kt
@@ -2,7 +2,10 @@ package com.neppplus.daily10minutes_apiserverpractice_20210410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.neppplus.daily10minutes_apiserverpractice_20210410.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
@@ -22,7 +25,31 @@ class SignUpActivity : BaseActivity() {
             val inputPw = passwordEdt.text.toString()
             val inputNickname = nicknameEdt.text.toString()
 
+            ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
 
+                    val code = jsonObj.getInt("code")
+
+                    if (code == 200) {
+
+                        runOnUiThread {
+
+                            Toast.makeText(mContext, "환영합니다.", Toast.LENGTH_SHORT).show()
+                            finish()
+
+                        }
+
+                    }
+                    else {
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+            })
 
         }
 
