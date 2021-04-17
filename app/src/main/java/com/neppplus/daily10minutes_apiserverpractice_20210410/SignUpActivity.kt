52f20e5cefbb5f4e199742c29a60497bc09073f5
홍준호 <0615wnsgh@gmail.com>
2021-04-17 10:31:00 +0900

    EditText문구 변경 이벤트 체험

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/SignUpActivity.kt
@@ -2,6 +2,9 @@ package com.neppplus.daily10minutes_apiserverpractice_20210410
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.Toast
 import com.neppplus.daily10minutes_apiserverpractice_20210410.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_sign_up.*
@@ -19,6 +22,23 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        emailEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                Log.d("문구변경이벤트", s.toString())
+
+                emailCheckResultTxt.text = "이메일 중복 확인을 해주세요."
+            }
+
+        })
+
         emailCheckBtn.setOnClickListener {
 
 //            입력한 이메일을 받아서
@@ -26,7 +46,20 @@ class SignUpActivity : BaseActivity() {
 
 //            서버에 중복인지 아닌지 물어보는 기능 API 활용. => 그 결과를 텍스트뷰에 반영 (UI 반영)
 
-            ServerUtil.getRequestEmailCheck(inputEmail, null)
+            ServerUtil.getRequestEmailCheck(inputEmail, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+
+                    runOnUiThread {
+                        if (code == 200) {
+                            emailCheckResultTxt.text = "사용해도 좋은 이메일입니다."
+                        } else {
+                            emailCheckResultTxt.text = "중복된 이메일이라 사용할 수 없습니다."
+                        }
+                    }
+                }
+            })
 
         }
 
@@ -36,31 +69,34 @@ class SignUpActivity : BaseActivity() {
             val inputPw = passwordEdt.text.toString()
             val inputNickname = nicknameEdt.text.toString()
 
-            ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.JsonResponseHandler {
-                override fun onResponse(jsonObj: JSONObject) {
+            ServerUtil.putRequestSignUp(
+                inputEmail,
+                inputPw,
+                inputNickname,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(jsonObj: JSONObject) {
 
-                    val code = jsonObj.getInt("code")
+                        val code = jsonObj.getInt("code")
 
-                    if (code == 200) {
+                        if (code == 200) {
 
-                        runOnUiThread {
+                            runOnUiThread {
 
-                            Toast.makeText(mContext, "환영합니다.", Toast.LENGTH_SHORT).show()
-                            finish()
+                                Toast.makeText(mContext, "환영합니다.", Toast.LENGTH_SHORT).show()
+                                finish()
 
+                            }
+
+                        } else {
+                            val message = jsonObj.getString("message")
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
                         }
 
                     }
-                    else {
-                        val message = jsonObj.getString("message")
-                        runOnUiThread {
-                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
-                        }
-                    }
 
-                }
-
-            })
+                })
 
         }
 
