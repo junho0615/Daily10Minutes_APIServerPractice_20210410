8b1189ebe40c21a2ec521a185bf6ecb6b0427332
홍준호 <0615wnsgh@gmail.com>
2021-04-17 14:41:00 +0900

    프로젝트의 태그목록> 동적 추가

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/datas/Project.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/datas/Project.kt
@@ -11,6 +11,10 @@ class Project(
     var onGoingUserCount : Int,
     var proofMethod : String) : Serializable {
 
+//    태그 목록을 저장하기위한 ArrayList
+    val tags = ArrayList<String>()
+
+
 //    보조 생성자 추가. => Project() 만으로도 만들 수 있게.
     constructor() : this(0, "", "", "", 0, "")
 
@@ -25,6 +29,14 @@ class Project(
 
             project.onGoingUserCount = jsonObj.getInt("ongoing_users_count")
             project.proofMethod = jsonObj.getString("proof_method")
+
+            val tagsArr = jsonObj.getJSONArray("tags")
+
+            for (index in 0 until tagsArr.length()) {
+                val tagObj = tagsArr.getJSONObject(index)
+                val title = tagObj.getString("title")
+                project.tags.add(title)
+            }
 
             return project
         }
