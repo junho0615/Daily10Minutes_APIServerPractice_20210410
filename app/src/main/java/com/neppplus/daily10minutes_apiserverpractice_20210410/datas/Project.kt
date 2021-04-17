5f17f3ccdaca6a47fdbe8c5c368a6b83bc8555de
홍준호 <0615wnsgh@gmail.com>
2021-04-17 12:14:28 +0900

    JSONArray -> JSONOBject 추출 -> Project변환

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/datas/Project.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/datas/Project.kt
@@ -1,8 +1,26 @@
 package com.neppplus.daily10minutes_apiserverpractice_20210410.datas
 
+import org.json.JSONObject
+
 class Project(
     var id : Int,
     var title : String,
     var imageUrl : String,
     var description : String) {
+
+//    보조 생성자 추가. => Project() 만으로도 만들 수 있게.
+    constructor() : this(0, "", "", "")
+
+    companion object {
+        fun getProjectFromJson(jsonObj : JSONObject) : Project {
+            val project = Project()
+
+            project.id = jsonObj.getInt("id")
+            project.title = jsonObj.getString("title")
+            project.imageUrl = jsonObj.getString("imageUrl")
+            project.description = jsonObj.getString("description")
+
+            return project
+        }
+    }
 }
