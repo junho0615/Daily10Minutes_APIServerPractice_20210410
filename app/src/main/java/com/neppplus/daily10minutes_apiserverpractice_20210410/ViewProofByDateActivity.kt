547d8f321458bf79f48efddb02e94fa78f919f67
홍준호 <0615wnsgh@gmail.com>
2021-04-24 10:36:24 +0900

    Calendar 활용 체험  - 객체화를 생성자 이외의 방법.  - 해당 날짜의 get 기능 : 항목별 데이터 조회

--- app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/ViewProofByDateActivity.kt
+++ app/src/main/java/com/neppplus/daily10minutes_apiserverpractice_20210410/ViewProofByDateActivity.kt
@@ -6,9 +6,13 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.DatePicker
 import kotlinx.android.synthetic.main.activity_view_proof_by_date.*
+import java.util.*
 import kotlin.math.log
 
 class ViewProofByDateActivity : BaseActivity() {
+
+    val mSeletedDate = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_proof_by_date)
@@ -30,7 +34,10 @@ class ViewProofByDateActivity : BaseActivity() {
             }
 //            실제 달력 띄우기 (AlertDialLog와 유사)
 
-            val datePickerDialog = DatePickerDialog(mContext, dateSetListener, 2021, 4, 24)
+            val datePickerDialog = DatePickerDialog(mContext, dateSetListener,
+                mSeletedDate.get(Calendar.YEAR),
+                mSeletedDate.get(Calendar.MONTH),
+                mSeletedDate.get(Calendar.DAY_OF_MONTH))
             datePickerDialog.show()
         }
 
